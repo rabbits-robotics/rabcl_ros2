2d0b22517f8ae829dcb4ab10b381031c027dae1f
kota0314 <kota0314yama@gmail.com>
2025-11-02 16:26:00 +0900

    msg型の変更に適用

--- src/utils.cpp
+++ src/utils.cpp
@@ -4,15 +4,15 @@ namespace rabcl_ros2
 {
 void Utils::CmdMsgToInfo(const attracts_msgs::msg::AttractsCommand& msg, rabcl::Info& info)
 {
-    info.chassis_vel_x_ = msg.chassis_vel_x.data;
-    info.chassis_vel_y_ = msg.chassis_vel_y.data;
-    info.chassis_vel_z_ = msg.chassis_vel_z.data;
-    info.pitch_vel_ = msg.pitch_vel.data;
-    info.yaw_vel_ = msg.yaw_vel.data;
+    info.chassis_vel_x_ = msg.chassis_vel.x;
+    info.chassis_vel_y_ = msg.chassis_vel.y;
+    info.chassis_vel_z_ = msg.chassis_vel.z;
+    info.pitch_vel_ = msg.pitch_pos;
+    info.yaw_vel_ = msg.yaw_pos;
 
-    info.load_mode_ = msg.load_mode.data;
-    info.fire_mode_ = msg.fire_mode.data;
-    info.speed_mode_ = msg.speed_mode.data;
-    info.chassis_mode_ = msg.chassis_mode.data;
+    info.load_mode_ = msg.load_mode;
+    info.fire_mode_ = msg.fire_mode;
+    info.speed_mode_ = msg.speed_mode;
+    info.chassis_mode_ = msg.chassis_mode;
 }
 } // namespace rabcl
