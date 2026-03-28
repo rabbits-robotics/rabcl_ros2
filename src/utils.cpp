58e3f908cf184bfbca04355ee1d90b70e9f37cf8
kota0314 <kota0314yama@gmail.com>
2026-03-28 21:09:51 +0900

    スタイル修正（インクルードガード・cmath・名前空間コメント・整形）

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,17 +2,18 @@
 
 namespace rabcl_ros2
 {
-void Utils::CmdMsgToInfo(const attracts_msgs::msg::AttractsCommand& msg, rabcl::Info& info)
+void Utils::CmdMsgToInfo(
+  const attracts_msgs::msg::AttractsCommand & msg, rabcl::Info & info)
 {
-    info.chassis_vel_x_ = msg.chassis_vel.x;
-    info.chassis_vel_y_ = msg.chassis_vel.y;
-    info.chassis_vel_z_ = msg.chassis_vel.z;
-    info.pitch_pos_ = msg.pitch_pos;
-    info.yaw_pos_ = msg.yaw_pos;
+  info.chassis_vel_x_ = msg.chassis_vel.x;
+  info.chassis_vel_y_ = msg.chassis_vel.y;
+  info.chassis_vel_z_ = msg.chassis_vel.z;
+  info.pitch_pos_ = msg.pitch_pos;
+  info.yaw_pos_ = msg.yaw_pos;
 
-    info.load_mode_ = msg.load_mode;
-    info.fire_mode_ = msg.fire_mode;
-    info.speed_mode_ = msg.speed_mode;
-    info.chassis_mode_ = msg.chassis_mode;
+  info.load_mode_ = msg.load_mode;
+  info.fire_mode_ = msg.fire_mode;
+  info.speed_mode_ = msg.speed_mode;
+  info.chassis_mode_ = msg.chassis_mode;
 }
-} // namespace rabcl
+}  // namespace rabcl_ros2
